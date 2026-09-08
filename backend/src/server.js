19f08a791a4b33d75f1a5c778f8fc1642109a1e9
Aditya Dabhade <adabhade087@gmail.com>
2026-09-08 16:30:10 +0530

    Fix database config path

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const db = require("./config/db");
+const db = require("../config/db");
 const repositoryRoutes = require("../routes/repositoryRoutes");
 
 const app = express();
